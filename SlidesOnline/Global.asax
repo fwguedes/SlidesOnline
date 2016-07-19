b6a2c60665c1efb4f70f7343ff5b3238fcae3d2a
Avell <Avell@AVELL-155-V3>
2016-07-19 20:55:50 -0300

    Add project files.

---
+++ SlidesOnline/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SlidesOnline.MvcApplication" Language="C#" %>
---
+++ SlidesOnline/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SlidesOnline.Startup))]
+namespace SlidesOnline
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
